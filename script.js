b9d6109a8cca67f17a780aa7aeb77ca6f0b07e97
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-08 03:17:13 +0800

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-const menuBtn = document.querySelector('.menu')
-const nav = document.querySelector('.nav')
+const menuBtn = document.querySelector('.menu');
+const navLinks = document.querySelectorAll('.navbar a');
+const nav = document.querySelector('.nav');
+const dropdowns = document.querySelectorAll('.dropdown');
 
 menuBtn.addEventListener('click', () => {
-    nav.classList.toggle('show-nav')
-})
+    nav.classList.toggle('show-nav');
+});
+
+navLinks.forEach((navLink) => {
+    navLink.addEventListener('mouseover', () => {
+        const dropdownId = navLink.getAttribute('data-dropdown');
+        const dropdown = document.getElementById(dropdownId);
+
+        if (dropdown) {
+            dropdown.classList.add('show');
+        }
+    });
+
+    navLink.addEventListener('mouseleave', () => {
+        const dropdownId = navLink.getAttribute('data-dropdown');
+        const dropdown = document.getElementById(dropdownId);
+
+        if (dropdown) {
+            dropdown.classList.remove('show');
+        }
+    });
+});
