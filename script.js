14a79ab5288a88348ab7b720708bb12c39c42b70
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-08 00:07:57 +0800

    added menu

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const menuBtn = do
+const menuBtn = document.querySelector('.menu')
+const nav = document.querySelector('.nav')
+
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('show-nav')
+})
